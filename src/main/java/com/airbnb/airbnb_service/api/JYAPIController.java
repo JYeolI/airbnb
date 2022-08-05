f4ab3f28073998095e0e2adb65bd9bff461c29d1
JY <whddbs6753@naver.com>
2022-08-05 17:51:53 +0900

    mian 검색바 검색필터 작업1차 완료

--- src/main/java/com/airbnb/airbnb_service/api/JYAPIController.java
+++ src/main/java/com/airbnb/airbnb_service/api/JYAPIController.java
@@ -1,6 +1,7 @@
 package com.airbnb.airbnb_service.api;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.airbnb.airbnb_service.data.MainViewResponse;
 import com.airbnb.airbnb_service.mapper.JyTempMapper;
 
 @RestController
@@ -17,19 +19,34 @@ import com.airbnb.airbnb_service.mapper.JyTempMapper;
 public class JYAPIController {
     @Autowired JyTempMapper mapper;
 
+
+    // 메인화면 숙소 검색조회
     @GetMapping("/main/search")
     public Map<String,Object> getMainViewData(
-        @RequestParam @Nullable String keyword,        
-        @RequestParam @Nullable Integer checkIn,
-        @RequestParam @Nullable Integer checkOut,
-        @RequestParam @Nullable Integer Guest,
-        @RequestParam @Nullable Integer dog
+        @RequestParam @Nullable String in_dt, 
+        @RequestParam @Nullable String out_dt,
+        @RequestParam @Nullable Integer cate_place, 
+        @RequestParam @Nullable Integer lang,
+        @RequestParam @Nullable Integer amenity, 
+        @RequestParam @Nullable Integer guest, 
+        @RequestParam @Nullable Integer dog,
+        @RequestParam @Nullable Integer min, 
+        @RequestParam @Nullable Integer max, 
+        @RequestParam @Nullable Integer type,
+        @RequestParam @Nullable Integer bed, 
+        @RequestParam @Nullable Integer bedroom, 
+        @RequestParam @Nullable Integer bathroom, 
+        @RequestParam @Nullable Integer superhost
     ) {
         Map<String,Object> resultMap = new LinkedHashMap<String, Object>();
-        resultMap.put("searchHouseList",mapper.selectSearchHouseList());
-        
 
-
+        //임시회원번호
+        Integer user_seq =1;
+        List<MainViewResponse> list = mapper.selectSearchHouseList(
+            user_seq, in_dt, out_dt, cate_place, lang, amenity, 
+            guest, dog, min, max, type, bed, bedroom, bathroom, superhost
+        );
+        resultMap.put("searchHouseList",list);
         
         return resultMap;
     }
--- src/main/java/com/airbnb/airbnb_service/mapper/JyTempMapper.java
+++ src/main/java/com/airbnb/airbnb_service/mapper/JyTempMapper.java
@@ -8,5 +8,12 @@ import com.airbnb.airbnb_service.data.MainViewResponse;
 
 @Mapper
 public interface JyTempMapper {
-    public List<MainViewResponse> selectSearchHouseList();
+    public List<MainViewResponse> selectSearchHouseList(
+        Integer user_seq, String in_dt, String out_dt,
+        Integer cate_place, Integer lang,
+        Integer amenity, Integer guest, Integer dog,
+        Integer min, Integer max, Integer type,
+        Integer bed, Integer bedroom, Integer bathroom, 
+        Integer superhost
+    );
 }

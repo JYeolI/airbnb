1d5395798a22621cfe071d36215c2a3ccb76c5a3
yunsig kang <gang1428@naver.com>
2022-08-05 19:13:38 +0900

    error 수정

--- src/main/java/com/airbnb/airbnb_service/mapper/JyTempMapper.java
+++ src/main/java/com/airbnb/airbnb_service/mapper/JyTempMapper.java
@@ -11,22 +11,11 @@ import com.airbnb.airbnb_service.data.category.CategoryLangVO;
 
 @Mapper
 public interface JyTempMapper {
-<<<<<<< HEAD
+
     public List<MainViewResponse> selectSearchHouseList(SearchRequestVO searchRequest, Integer user_seq);
     public List<CategoryAmenityVO> selectCategoryAmenityList();
     public List<CategoryLangVO> selectCategoryLangList();
-=======
-<<<<<<< HEAD
-    public List<MainViewResponse> selectSearchHouseList(
-        Integer user_seq, String in_dt, String out_dt,
-        Integer cate_place, Integer lang,
-        Integer amenity, Integer guest, Integer dog,
-        Integer min, Integer max, Integer type,
-        Integer bed, Integer bedroom, Integer bathroom, 
-        Integer superhost
-    );
-=======
-    public List<MainViewResponse> selectSearchHouseList(Integer start_dt, Integer end_dt, String keyword, Integer guest, Integer dog);
->>>>>>> b7e7e5f (test (work1_1 브랜치 삭제 예정))
->>>>>>> h_save2
+
+
+
 }

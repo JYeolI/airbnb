df198fa6b19034af03721d7b747ab7511a4bbbc2
JYeolI <hn05057@naver.com>
2022-08-08 18:38:26 +0900

    wq

---
+++ src/main/java/com/airbnb/airbnb_service/data/category/CategoryBarVO.java
@@ -0,0 +1,11 @@
+package com.airbnb.airbnb_service.data.category;
+
+import lombok.Data;
+
+@Data
+public class CategoryBarVO {
+    public Integer table_no;
+    public Integer cate_bar_seq;
+    public String cate_bar_content;
+    public String cate_bar_icon;
+}

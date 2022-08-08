1d39b2f54a9aae9808acd9e8ca89d8b5653529d6
yunsig kang <gang1428@naver.com>
2022-08-08 18:35:19 +0900

    develop merge`

--- src/main/java/com/airbnb/airbnb_service/data/category/CategorybarVO.java
+++ src/main/java/com/airbnb/airbnb_service/data/category/CategorybarVO.java
@@ -3,7 +3,7 @@ package com.airbnb.airbnb_service.data.category;
 import lombok.Data;
 
 @Data
-public class CategoryBarVO {
+public class CategorybarVO {
     public Integer table_no;
     public Integer cate_bar_seq;
     public String cate_bar_content;

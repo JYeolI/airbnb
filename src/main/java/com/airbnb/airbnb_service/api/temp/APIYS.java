a19ea41160665be8564d82d81377fc6dc77251f9
yunsig kang <gang1428@naver.com>
2022-08-09 14:45:00 +0900

    ys

--- src/main/java/com/airbnb/airbnb_service/api/temp/APIYS.java
+++ src/main/java/com/airbnb/airbnb_service/api/temp/APIYS.java
@@ -3,10 +3,11 @@ package com.airbnb.airbnb_service.api.temp;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.airbnb.airbnb_service.mapper.temp.tempTripMapper;
@@ -17,11 +18,13 @@ public class APIYS {
     @Autowired tempTripMapper temp_mapper;
     // 여행 예약
     @GetMapping("/trip_response")
-    public Map<String, Object> getTripResponse(@RequestParam Integer seq) {
+    public Map<String, Object> getTripResponse(HttpSession session) {
         Map<String,Object> resultMap=new LinkedHashMap<String, Object>();
-            resultMap.put("trip_list", temp_mapper.selectTripResponse(seq));
+            // MemberInfoVO user = (MemberInfoVO)(session.getAttribute("user"));
+            // Integer user_seq = user.getMi_seq();
+        Integer user_seq = 1;
+            resultMap.put("trip_list", temp_mapper.selectTripResponse(user_seq));
         return resultMap;
     }
-
 }
 
---
+++ src/main/java/com/airbnb/airbnb_service/controller/YS.java
@@ -0,0 +1,12 @@
+package com.airbnb.airbnb_service.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class YS {
+    @GetMapping("/ys")
+    public String getMain() {
+        return "/ys";
+    }
+}

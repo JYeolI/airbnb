087a647059d5280c028988234bc2a614fe6851b4
yunsig kang <gang1428@naver.com>
2022-08-09 10:18:37 +0900

    pull

--- src/main/java/com/airbnb/airbnb_service/data/category/CategoryCountryVO.java
+++
@@ -1,9 +0,0 @@
-package com.airbnb.airbnb_service.data.category;
-
-import lombok.Data;
-
-@Data
-public class CategoryCountryVO {
-    private Integer cc_seq;
-    private String cc_content;
-}
--- src/main/java/com/airbnb/airbnb_service/data/member/MemberImgVO.java
+++
@@ -1,9 +0,0 @@
-package com.airbnb.airbnb_service.data.member;
-
-import lombok.Data;
-
-@Data
-public class MemberImgVO {
-    private Integer mimg_seq;
-    private String mimg_file;
-}
